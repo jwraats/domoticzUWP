4bc132768e7299043e206f4baf99f0feb5fc961c
Jan Willem Raats <jw.raats@gmail.com>
2016-01-04 12:19:17 +0100

    Initial commit with Template 10

---
+++ DomoticzUWP/DomoticzUWP/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+using DomoticzUWP.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace DomoticzUWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Disabled;
+        }
+
+        // strongly-typed view models enable x:bind
+        public MainPageViewModel ViewModel => this.DataContext as MainPageViewModel;
+    }
+}
